7f1aa75c0b87e645d7fa7b914ab1bec214d4e491
amais23 <henry3.141592653589@gmail.com>
2026-09-18 09:33:12 +0800

    chore(release): bump version to v0.3.0

--- src/components/TitleMenu.tsx
+++ src/components/TitleMenu.tsx
@@ -165,7 +165,7 @@ export const TitleMenu: React.FC<TitleMenuProps> = ({
 
         {/* Footer info */}
         <footer className="title-menu-footer">
-          <span>v0.2.1 · ARKHAM INVESTIGATION DIVISION</span>
+          <span>v0.3.0 · ARKHAM INVESTIGATION DIVISION</span>
         </footer>
       </main>
     </div>
